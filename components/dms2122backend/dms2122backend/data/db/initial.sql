dc8b73faff6b83135af11c668eff49fc9d30966a
ChocoAlmendrado <pss1005@alu.ubu.es>
2021-11-24 13:15:36 +0100

    First Steps db

---
+++ components/dms2122backend/dms2122backend/data/db/initial.sql
@@ -0,0 +1,33 @@
+BEGIN TRANSACTION;
+CREATE TABLE Users (
+        username VARCHAR(32),
+        password VARCHAR(64) NOT NULL,
+        PRIMARY KEY (username)
+);
+CREATE TABLE Question(
+    id integer PRIMARY KEY,
+    statement varchar(250),
+    correctOption varchar(250),
+    --incorrectOptions json,
+    score float,
+    penalty float,
+    public boolean,
+    ncorrect integer,
+    nanswered integer
+);
+CREATE TABLE AnsweredQuestion(
+    idquestion integer, --FOREIGN KEY REFERENCES Question(id),
+    iduser integer,
+    answer varchar(250),
+    score float,
+    creation date,
+    PRIMARY KEY(idquestion, iduser),
+    FOREIGN KEY(idquestion) REFERENCES Question(id)
+);
+CREATE TABLE UserStats(
+    iduser varchar(32) PRIMARY KEY REFERENCES Users(id),
+    nanswered integer,
+    ncorrect integer,
+    score float
+);
+COMMIT;
