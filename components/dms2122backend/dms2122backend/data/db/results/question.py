feb86661b2456c06bc2b88b195d084367a7c3633
Jorge Ruiz <jorgelinirg@gmail.com>
2021-11-26 19:40:01 +0100

    Question Table Changes

--- components/dms2122backend/dms2122backend/data/db/results/question.py
+++ components/dms2122backend/dms2122backend/data/db/results/question.py
@@ -1,19 +1,29 @@
 """ Question class module.
 """
 
-from typing import Dict
+from typing import Dict, List
 from sqlalchemy import Table, MetaData, Column, String, Integer, Float, Boolean  # type: ignore
 from sqlalchemy.orm import relationship  # type: ignore
 from dms2122backend.data.db.results.resultbase import ResultBase
 from dms2122backend.data.db.results.userrole import UserRole
 from answeredQuestion import AnsweredQuestion
 
+
 class Question(ResultBase):
     """ Definition and storage of user ORM records.
     """
 
-    def __init__(self, title: str, statement: str, correctOption: str, 
-                 score: float=0, penalty: float=0, public: bool=True):
+    def __init__(
+        self,
+        title: str,
+        statement: str,
+        correctOption: str,
+        incorrectOptions: List[str],
+        imageUrl: str = "",
+        score: float = 0,
+        penalty: float = 0,
+        public: bool = True,
+    ):
         """ Constructor method.
 
         Initializes a user record.
@@ -25,7 +35,8 @@ class Question(ResultBase):
         self.title: str = title
         self.statement: str = statement
         self.correctOption: str = correctOption
-        #self.incorrectOptions: list[str] = incorrectOptions
+        self.incorrectOptions: List[str] = incorrectOptions
+        self.imageUrl: str = imageUrl
         self.score: float = score
         self.penalty: float = penalty
         self.public: bool = public
@@ -44,18 +55,19 @@ class Question(ResultBase):
             - Table: A `Table` object with the table definition.
         """
         return Table(
-                'questions',
-                metadata,
-                Column('title', String(250), primary_key=True),
-                Column('statement', String(250), nullable=False),
-                Column('correctOption', String(250), nullable=False),
-                #Column('incorrectOptions', json, nullable=False), # TODO: hacer json
-                Column('score', Float, default=0),
-                Column('penalty', Float, default=0),
-                Column('public', Boolean, default=True),
-                Column('ncorrect', Integer, default=0),
-                Column('nanswered', Integer, default=0)
-            )
+            "questions",
+            metadata,
+            Column("title", String(250), primary_key=True),
+            Column("statement", String(250), nullable=False),
+            Column("imageUrl", String(250), nullable=True),
+            Column("correctOption", String(250), nullable=False),
+            Column("incorrectOptions", String(), nullable=False),
+            Column("score", Float, default=0),
+            Column("penalty", Float, default=0),
+            Column("public", Boolean, default=True),
+            Column("ncorrect", Integer, default=0),
+            Column("nanswered", Integer, default=0),
+        )
 
     @staticmethod
     def _mapping_properties() -> Dict:
@@ -64,4 +76,6 @@ class Question(ResultBase):
         Returns:
             - Dict: A dictionary with the mapping properties.
         """
-        return {'answereds': relationship(AnsweredQuestion, backref='answeredQuestions')}
+        return {
+            "answereds": relationship(AnsweredQuestion, backref="answeredQuestions")
+        }
