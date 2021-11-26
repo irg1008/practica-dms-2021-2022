f7cba5a6d9cd17a671530b4e9432795920178a7e
ChocoAlmendrado <pss1005@alu.ubu.es>
2021-11-26 14:02:01 +0100

    example sql

--- components/dms2122backend/dms2122backend/data/db/initial.sql
+++ components/dms2122backend/dms2122backend/data/db/initial.sql
@@ -4,17 +4,19 @@ CREATE TABLE Users (
         password VARCHAR(64) NOT NULL,
         PRIMARY KEY (username)
 );
+
 CREATE TABLE Question(
     id integer PRIMARY KEY,
     statement varchar(250),
     correctOption varchar(250),
-    --incorrectOptions json,
+    incorrectOptions varchar,
     score float,
     penalty float,
     public boolean,
     ncorrect integer,
     nanswered integer
 );
+
 CREATE TABLE AnsweredQuestion(
     idquestion integer, --FOREIGN KEY REFERENCES Question(id),
     iduser integer,
@@ -24,10 +26,12 @@ CREATE TABLE AnsweredQuestion(
     PRIMARY KEY(idquestion, iduser),
     FOREIGN KEY(idquestion) REFERENCES Question(id)
 );
+
 CREATE TABLE UserStats(
     iduser varchar(32) PRIMARY KEY REFERENCES Users(id),
     nanswered integer,
     ncorrect integer,
     score float
 );
+
 COMMIT;
