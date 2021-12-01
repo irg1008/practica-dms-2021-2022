b7f06f99e4c02fc2c2f008a2c2aa276654827b00
Jorge <jrg1011@alu.ubu.es>
2021-12-01 21:14:50 +0100

    Cosas

--- components/dms2122backend/dms2122backend/presentation/rest/question.py
+++ components/dms2122backend/dms2122backend/presentation/rest/question.py
@@ -1,5 +1,5 @@
 from typing import Dict, Optional, Tuple
-
+from http import HTTPStatus
 from flask.globals import current_app
 
 from dms2122backend.service.auth.protected_endpoint_dec import protected_endpoint
@@ -16,7 +16,7 @@ def new(body: Dict, **kwargs) -> Tuple[int, Optional[int]]:
         Tuple[str, Optional[int]]: Response message and status code
     """
 
-    return (1, 200)
+    return (1, HTTPStatus.OK)
 
 @protected_endpoint(roles=[Role.Teacher, Role.Student])
 def getQ(id: int, **kwargs):
@@ -27,6 +27,6 @@ def editQ(id: int, **kwargs):
     return (id, 200)
 
 
-
+@protected_endpoint(roles=[Role.Teacher])
 def getAll(**kwargs):
     return ({}, 200)
--- components/dms2122frontend/dms2122frontend/data/rest/database_client/restDB.py
+++ components/dms2122frontend/dms2122frontend/data/rest/database_client/restDB.py
@@ -1,47 +1,69 @@
-from typing import List, Tuple, Union
 
-from dms2122frontend.data.Question import Question, AnsweredQuestion
-from abc import ABC, abstractmethod
+from typing import Dict, List, Tuple, Union
 
-class DatabaseClient(ABC):
-    """
-    Databse Adapter Interface
-    """
+import requests
+from dms2122frontend.data.Question import Question
+from dms2122frontend.data.Question import AnsweredQuestion
+from dms2122frontend.data.rest.database_client.database_client import DatabaseClient
 
-    @abstractmethod
+
+class RestDB(DatabaseClient):
+
+    def __init__(self, base_url: str, api_back_secret: str,api_back_header: str) -> None:
+        self.__base_url:str = base_url
+        self.__auth_secret:str = api_back_secret
+        self.__api_back_header: str = api_back_header
+
+    def __check_token(self, token: str):
+        if len(token) == 0 or len(token.split(".")) != 3:
+            raise Exception("Invalid Token Format")
+
+    def __get_header(self, token: str) -> Dict:
+        return  {"Authorization": f"Bearer {token}",
+                self.__api_back_header: self.__api_back_header}
+
     def getAnsweredQuestions(self, username: str, token="") -> List[AnsweredQuestion]:
         raise Exception("Not Implemented")
 
-    @abstractmethod
+    
     def getUnasweredQuestions(self, username: str, token="") -> List[Question]:
         raise Exception("Not Implemented")
         return []
-    @abstractmethod
+
     def answerQuestion(self, username: str, question_id: int, answer: str, token="") -> bool:
         raise Exception("Not Implemented")
     
-    @abstractmethod
+
     def getQuestion(self, question_id: int, token="") -> Union[Question, None]:
         raise Exception("Not Implemented")
         
-    @abstractmethod
+
     def getCurrentQuestionId(self, token="") -> int:
         raise Exception("Not Implemented")
 
-    @abstractmethod
+
     def createQuestion(self, question: Question, token="") -> Tuple[Question, int]:
         raise Exception("Not Implemented")
 
-    @abstractmethod
+
     def updateQuestion(self, Question: Question, token="") -> None:
         raise Exception("Not Implemented")
 
-    @abstractmethod
+
     def getAnsweredQuestion(
         self, username: str, question_id: int, token=""
     ) -> Union[AnsweredQuestion, None]:
         raise Exception("Not Implemented")
+        
 
-    @abstractmethod
     def getAllQuestions(self, token="") -> List[Question]:
-        raise Exception("Not Implemented")
+        
+        self.__check_token(token)
+
+        res = requests.get(f"{self.__base_url}/question/all", self.__get_header(token))
+
+        if not res.ok:
+            return []
+
+
+        return []
